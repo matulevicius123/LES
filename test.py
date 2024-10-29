854a2a055ae265ae73b15da9740f64d17b3a257c
matulevicius123 <142500460+matulevicius123@users.noreply.github.com>
2024-10-29 17:38:00 -0300

    Atualizando testes

--- test.py
+++ test.py
@@ -1,19 +1,16 @@
 import pytest
 from flask import url_for
-from app import app as flask_app  # Importa o app do seu arquivo
+from testapp import test app as flask_app  # Importa o app do seu arquivo
 from extensions import db
 from models import User
 
 @pytest.fixture
 def app():
-    print('')
-    # flask_app.config['TESTING'] = True
-    # flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # Usar banco de dados em memória para testes
-    # with flask_app.app_context():
-    #     db.create_all()  # Cria as tabelas no banco de dados de teste
-    #     yield flask_app
-    #     db.session.remove()
-    #     db.drop_all()  # Remove as tabelas após os testes
+    with flask_app.app_context():
+        db.create_all()  # Create tables in the in-memory database
+        yield flask_app  # Yield the test app for testing
+        db.session.remove()  # Remove any sessions
+        db.drop_all()  # Drop all tables after the tests are done
 
 @pytest.fixture
 def client(app):
@@ -21,8 +18,7 @@ def client(app):
 
 def test_login_get(client):
     response = client.get(url_for('login'))  # Faz um GET na rota de login
-    assert response.status_code == 200
-    assert 'Login' in response.data.decode('utf-8')  # Verifica se a página contém o texto 'Login'
+        assert b"Criar uma nova conta" in response.data
 
 def test_login_post_invalid(client):
  # Simula um POST com dados inválidos
